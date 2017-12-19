27030b20daae5250336e810ca814bb46ffef9cc9
Jason Chen <me@hybridword.com>
2017-12-19 10:27:59 +0800

    first commit

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="#">jason</a> 设计和编码
+        </p>
+
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
+    </div>
+</footer>
